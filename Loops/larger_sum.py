5b13b7dfa7a669e1eb06fd8390de1bfd0a40b3c3
Umesh-A <iumesh.ar@gmail.com>
2020-04-08 22:46:29 +0530

    Loop code challenges

---
+++ Loops/larger_sum.py
@@ -0,0 +1,16 @@
+#Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+#The function should return the list whose elements sum to the greater number. 
+#If the sum of the elements of each list are equal, return lst1.
+
+def larger_sum(lst1, lst2):
+  sum1 = 0 
+  sum2 = 0
+  for i in lst1:
+    sum1 += i
+  for j in lst2:
+    sum2 += j
+  if sum1 < sum2:
+    return lst2
+  else:
+    return lst1
+print(larger_sum([1, 9, 5], [2, 3, 7]))
